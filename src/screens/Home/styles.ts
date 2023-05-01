0fb592fd9bedab5e5abed515d465fc276ff6499b
ingridsj <ingridsjalmeida@gmail.com>
2023-05-01 14:15:48 -0300

    fix cor background

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#333333',
+    backgroundColor: '#1A1A1A',
   },
   header: {
     backgroundColor: '#0D0D0D',
