e69ea8c10e09788f94290de05ba83fb93556d522
ingridsj <ingridsjalmeida@gmail.com>
2023-05-01 12:40:56 -0300

    add input de cadastro de tarefa

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,26 @@
-import { View, Image } from "react-native";
+import { View, Image, TextInput, TouchableOpacity } from "react-native";
 
 import { styles } from "./styles";
 
 import logo from "../../assets/logo.png";
+import plus from "../../assets/plus.png";
 
 export function Home() {
   return (
-    <View style={styles.header}>
-      <Image source={logo} style={styles.logo}/>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logo} style={styles.logo} />
+      </View>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Adicione uma nova tarefa"
+          placeholderTextColor="#808080"
+        />
+        <TouchableOpacity style={styles.button}>
+          <Image source={plus} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,6 +1,10 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#333333',
+  },
   header: {
     backgroundColor: '#0D0D0D',
     height: 173,
@@ -10,5 +14,28 @@ export const styles = StyleSheet.create({
   logo: {
     height: 32, 
     width: 110,
+  },
+  form: {
+    flexDirection: 'row',
+    paddingHorizontal: 24,
+    marginTop: -30,
+  },
+  input: {
+    flex: 1,
+    backgroundColor: '#262626',
+    height: 54,
+    borderRadius: 6,
+    marginRight: 4,
+    fontSize: 16,
+    padding: 16,
+    color: '#F2F2F2',
+  },
+  button: {
+    backgroundColor: '#1E6F9F',
+    height: 52,
+    width: 52,
+    borderRadius: 6,
+    alignItems: 'center',
+    justifyContent: 'center',
   }
 })
